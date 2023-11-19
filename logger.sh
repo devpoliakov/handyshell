04103edf5792c30fd6a464718e9607b37e63123a
Yurii <kontynent@ua.fm>
2023-11-20 00:29:40 +0200

    Log create log file in condition

--- logger.sh
+++ logger.sh
@@ -2,10 +2,28 @@
 
 
 
-if [ -d "../logdir" ]
+
+if [ -d ../logdir ]
 then
 echo "Directory exist"
 else
 `mkdir ../logdir`
 echo "Directory created"
+fi
+
+
+if [ -d ../logdir/$(date +'%Y') ]
+then
+echo "Year directory exist"
+else
+`mkdir ../logdir/$(date +'%Y')`
+echo "Year directory created"
+fi
+
+if [ -f ../logdir/$(date +'%Y')/$(date +'%m').log ]
+then
+echo "Month log exist"
+else
+`touch ../logdir/$(date +'%Y')/$(date +'%m').log`
+echo "Month log created"
 fi
